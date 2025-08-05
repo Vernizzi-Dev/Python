7a82d4f64411139a3b94a5a0538f465e0a91e94f
Victor Vernizzi de Oliveira <victor.vernizzi01@gmail.com>
2025-08-05 13:49:18 -0300

    acabei de criar o meu exercício que é sobre operadores de comparação

---
+++ Exercicios/Ex007/operadores_comparacao.py
@@ -0,0 +1,9 @@
+remuneracao = 2.500;
+gastos = 1.500;
+
+print(remuneracao == gastos);
+print(remuneracao != gastos);
+print(remuneracao > gastos);
+print(remuneracao >= gastos);
+print(remuneracao < gastos);
+print(remuneracao <= gastos);
