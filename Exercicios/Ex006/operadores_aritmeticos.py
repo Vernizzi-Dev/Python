613ac3582df356ba37de64ae3fb5435c34a5314b
Victor Vernizzi de Oliveira <victor.vernizzi01@gmail.com>
2025-08-05 13:44:30 -0300

    criei o meu exercício 6 que é sobre operadores atitméticos

---
+++ Exercicios/Ex006/operadores_aritmeticos.py
@@ -0,0 +1,28 @@
+""" adição: """
+print("Adição: 1 + 1 = ", 1 + 1)
+
+""" subtração: """
+print("Subtração: 20 - 10 = ", 20 - 10)
+
+""" multiplicação: """
+print("Multiplicação: 4 x 3 = ", 4 * 3)
+
+""" divisão: """
+print("Divisão: 20 / 10 = ", 20 / 10)
+
+""" divisão inteira: """
+print("Divisão Inteira: 12 / 2 = ", 12 // 2)
+
+""" módulo (resto da divisão): """
+print("Módulo: 10 % 3 = ", 10 % 3)
+
+""" exponenciação: """
+print("Exponenciação: 2 ** 3 = ", 2 ** 3)
+
+print("\n Ordem de execução na Matemática:")
+
+print("10 - 5 * 2 = ", 10 - 5 * 2)
+print("(10 - 5) * 2 = ", (10 - 5) * 2)
+print("10 ** 2 * 2 = ", 10 ** 2 * 2)
+print("10 ** (2 * 2) = ", 10 ** (2 * 2))
+print("10 / 2 * 4 = ", 10 / 2 * 4)
