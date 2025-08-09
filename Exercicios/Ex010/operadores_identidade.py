656b3e22129d7c8cdb18496d4dcd607d0f1f6774
Victor Vernizzi de Oliveira <victor.vernizzi01@gmail.com>
2025-08-08 23:40:36 -0300

    criei o meu exercício 10

---
+++ Exercicios/Ex010/operadores_identidade.py
@@ -0,0 +1,12 @@
+bootcamp = "Curso de Python";
+CURSO_NOME = bootcamp;
+
+print(bootcamp is CURSO_NOME);
+print(bootcamp is not CURSO_NOME);
+
+print("\n ================ \n")
+
+saldo, valor = 200, 200;
+
+print(saldo is valor)
+print(saldo is not valor)
